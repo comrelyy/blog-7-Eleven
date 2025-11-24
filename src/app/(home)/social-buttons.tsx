28d1394cbc6f52cdde5f8a918fb50153e0f16616
comrelyy <relyy2014@163.com>
2025-11-24 13:07:05 +0800

    Update GitHub link and comment out unused code

--- src/app/(home)/social-buttons.tsx
+++ src/app/(home)/social-buttons.tsx
@@ -42,7 +42,7 @@ export default function SocialButtons() {
 				<div className='absolute top-0 right-0 flex items-center gap-3 max-sm:static'>
 					{tertiaryShow && (
 						<motion.a
-							href='https://github.com/yysuni'
+							href='https://github.com/comrelyy'
 							target='_blank'
 							initial={{ opacity: 0, scale: 0.6 }}
 							animate={{ opacity: 1, scale: 1 }}
@@ -56,32 +56,32 @@ export default function SocialButtons() {
 					)}
 
 					{secondaryShow && (
-						<motion.a
-							href='https://juejin.cn/user/2427311675422382'
-							target='_blank'
-							initial={{ opacity: 0, scale: 0.6 }}
-							animate={{ opacity: 1, scale: 1 }}
-							whileHover={{ scale: 1.05 }}
-							whileTap={{ scale: 0.95 }}
-							className='card relative flex items-center gap-2 rounded-xl px-3 py-2.5 font-medium whitespace-nowrap'>
-							<JuejinSVG className='h-6 w-6' />
-							稀土掘金
-						</motion.a>
+//						<motion.a
+//							href='https://juejin.cn/user/2427311675422382'
+//							target='_blank'
+//							initial={{ opacity: 0, scale: 0.6 }}
+//							animate={{ opacity: 1, scale: 1 }}
+//							whileHover={{ scale: 1.05 }}
+//							whileTap={{ scale: 0.95 }}
+//							className='card relative flex items-center gap-2 rounded-xl px-3 py-2.5 font-medium whitespace-nowrap'>
+//							<JuejinSVG className='h-6 w-6' />
+//							稀土掘金
+						</motion.a> 
 					)}
 
-					<motion.button
-						onClick={() => {
-							navigator.clipboard.writeText('yysuni1001@gmail.com').then(() => {
-								toast.success('邮箱已复制到剪贴板')
-							})
-						}}
-						initial={{ opacity: 0, scale: 0.6 }}
-						animate={{ opacity: 1, scale: 1 }}
-						whileHover={{ scale: 1.05 }}
-						whileTap={{ scale: 0.95 }}
-						className='card btn relative rounded-xl p-1.5'>
-						<EmailSVG />
-					</motion.button>
+//					<motion.button
+//						onClick={() => {
+//							navigator.clipboard.writeText('yysuni1001@gmail.com').then(() => {
+//								toast.success('邮箱已复制到剪贴板')
+//							})
+//						}}
+//						initial={{ opacity: 0, scale: 0.6 }}
+//						animate={{ opacity: 1, scale: 1 }}
+//						whileHover={{ scale: 1.05 }}
+//						whileTap={{ scale: 0.95 }}
+//						className='card btn relative rounded-xl p-1.5'>
+//						<EmailSVG />
+//					</motion.button>
 				</div>
 			</motion.div>
 		)
