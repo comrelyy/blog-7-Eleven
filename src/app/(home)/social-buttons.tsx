bf0a2de1809e36d5b6502a207313a88248265ed1
comrelyy <relyy2014@163.com>
2025-11-24 13:12:39 +0800

    Comment out unused JSX code
    Comment out unused JSX code in social-buttons.tsx

--- src/app/(home)/social-buttons.tsx
+++ src/app/(home)/social-buttons.tsx
@@ -66,7 +66,7 @@ export default function SocialButtons() {
 //							className='card relative flex items-center gap-2 rounded-xl px-3 py-2.5 font-medium whitespace-nowrap'>
 //							<JuejinSVG className='h-6 w-6' />
 //							稀土掘金
-						</motion.a> 
+//						</motion.a> 
 					)}
 
 //					<motion.button
